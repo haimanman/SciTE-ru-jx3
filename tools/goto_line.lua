c69e9810f3491e47de1c3c531ee0089c468294fe
haimanman <haiman@czxiu.com>
2014-02-18 16:50:10 +0800

    初始化 Scite-ru 3.2.5 剑三定制版

---
+++ tools/goto_line.lua
@@ -0,0 +1,33 @@
+--[[--------------------------------------------------
+goto_line.lua
+Author: mozers™
+version: 1.1.1
+------------------------------------------------------
+При переходе на линию, текущее положение курсора на экране сохраняется
+для чего текст прокручивается на нужное количество строк.
+Теперь вам не придется искать курсор по всему экрану!
+(Реализация Issue 21 http://code.google.com/p/scite-ru/issues/detail?id=21)
+
+  Connection:
+   In file SciTEStartup.lua add a line:
+      dofile (props["SciteDefaultHome"].."\\tools\\goto_line.lua")
+--]]--------------------------------------------------
+local bypass = false
+
+local function GotoLine(line)
+	bypass = true
+	local linecur = editor:LineFromPosition(editor.CurrentPos)
+	local linecur_onscreen = linecur - editor.FirstVisibleLine
+	editor:GotoLine(line)
+	local line_onscreen = line - editor.FirstVisibleLine
+	if line_onscreen ~= linecur_onscreen then
+		editor:LineScroll(0, line_onscreen - linecur_onscreen)
+	end
+	bypass = false
+end
+
+AddEventHandler("OnSendEditor", function(id_msg, wp, lp)
+	if id_msg == SCI_GOTOLINE and not bypass then
+		GotoLine(wp)
+	end
+end)
